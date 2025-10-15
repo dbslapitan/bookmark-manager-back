17f140298b8cb7f465997dd26f6939dd793951c5
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-10-15 17:07:09 -0400

    add connection to mongodb

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,16 @@ if(result.error){
 }
 
 import * as express from "express";
-import {normalize} from "./utils/port";
+import { normalize } from "./utils/port";
+import { connect, connection } from "mongoose";
 
+const DB = process.env.DB;
 const app = express();
+
+connect(DB).catch(error => {
+  console.log(error);
+});
+connection.on("connected", () => console.log("Successfully connected to MongoDB..."));
 
 app.use("/", (req, res) => {
   res.json("Backend solution for Bookmark Manager from FrontEnd Mentor by Dirk Brandon Lapitan...");
